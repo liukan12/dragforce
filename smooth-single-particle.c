8433c8f89d143bcc6061644a19cb2263cf1e21b4
liukan12 <liukan-12@163.com>
2018-03-22 00:24:11 +0800

    Update smooth-single-particle.c

--- smooth-single-particle.c
+++ smooth-single-particle.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-#include <math.h>
-/*#include "udf.h" (used for FLUENT UDF)*/
-
-#define doulbe_precision 1
-
-#if (double_precision==1)
-  #define real double
-#else
-  #define real single
-#endif
-
-#define AIR_VISOCOSITY 1.83e-5 /*based on 300K */
-#define AIR_DEN 1.225
-
+#include "drag_global.h"
 
 /*calculate for Re number based on pure FLUID*/
 real RE_FLUID(real fluid_vis,real fluid_vel, real fluid_den，real  characteristic_length)
