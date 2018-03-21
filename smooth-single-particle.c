ba804e8fadcf892345fcf54b800f9e77be8f3ca1
liukan12 <liukan-12@163.com>
2018-03-22 00:27:44 +0800

    Update smooth-single-particle.c

--- smooth-single-particle.c
+++ smooth-single-particle.c
@@ -1,16 +1,4 @@
-#include "drag_global.h"
-
-/*calculate for Re number based on pure FLUID*/
-real RE_FLUID(real fluid_vis,real fluid_vel, real fluid_den，real  characteristic_length)
-{
-  return (fluid_vel*fluid_den*characteristic_length/fluid_vis);
-}
-
-/*calculate for Re number based on particle flow*/
-real REN_PFLUID(real fluid_vis, real fluid_vel, real par_vel, real par_diam)
-{
-  return (fabs(fluid_vel-par_vel)*fluid_den*par_diam/fluid_vis);
-}
+#include "Re_number.h"
 
 /*smooth sphere particles in the normal flow. \
 theory form the paper " An investigation of particle trajectories in two-phase flow systems"*/
