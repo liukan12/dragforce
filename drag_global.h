c69282b83aade1d80ada070ccb399c4434c40447
liukan12 <liukan-12@163.com>
2018-03-22 00:23:28 +0800

    Update drag_global.h

--- drag_global.h
+++ drag_global.h
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+/*#include "udf.h" (used for FLUENT UDF)*/
 
+#define doulbe_precision 1
+
+#if (double_precision==1)
+  #define real double
+#else
+  #define real single
+#endif
+
+#define AIR_VISOCOSITY 1.83e-5 /*based on 300K */
+#define AIR_DEN 1.225
