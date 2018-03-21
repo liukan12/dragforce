c13212ffc8beb2330dd0cb36c83ae3efccba0f32
liukan12 <liukan-12@163.com>
2018-03-22 00:27:11 +0800

    Update Re_number.c

--- Re_number.c
+++ Re_number.c
@@ -1,1 +1,14 @@
+#include "Re_number.h"
 
+
+/*calculate for Re number based on pure FLUID*/
+real RE_FLUID(real fluid_vis,real fluid_vel, real fluid_den，real  characteristic_length)
+{
+  return (fluid_vel*fluid_den*characteristic_length/fluid_vis);
+}
+
+/*calculate for Re number based on particle flow*/
+real REN_PFLUID(real fluid_vis, real fluid_vel, real par_vel, real par_diam)
+{
+  return (fabs(fluid_vel-par_vel)*fluid_den*par_diam/fluid_vis);
+}
